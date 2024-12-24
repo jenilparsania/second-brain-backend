78505305995ad873686175753e89999537268ea9
jenilparsania <151541448+jenilparsania@users.noreply.github.com>
2024-12-24 14:51:45 -0400

    sign up and sign in endpoint

---
+++ src/db.ts
@@ -0,0 +1,17 @@
+// mongo connection string : mongodb+srv://parsaniajenil:firstdb@cluster0.46eul.mongodb.net/
+import mongoose, {model,Schema} from "mongoose";
+mongoose.connect("mongodb+srv://parsaniajenil:firstdb@cluster0.46eul.mongodb.net/brainly")
+const UserSchema = new Schema({
+    username : {type : String , unique : true},
+    password : String
+})
+
+export const UserModel = model("User" , UserSchema);
+
+const ContentSchema = new Schema({
+    title : String,
+    link : String,
+    tags : [{type : mongoose.Types.ObjectId , ref : 'Tag'}],
+    userId : {type : mongoose.Types.ObjectId , ref : 'User', required : true},
+})
+
--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,60 @@
 // @tsignore would fix it but we mostly do not do that 
 // npm install -d @types/express
 // -d means , dev dependency. 
+// if you make any changes in the ts file then run build command first and then run the start command 
 import express from "express";
 import mongoose from "mongoose";
 import jwt from "jsonwebtoken";
+import { UserModel } from "./db";
 
 const app = express();
+app.use(express.json())
 
-app.post("/api/v1/signin",(req,res)=>{
-    
+const JWT_PASSWORD = "123123";
+
+app.post("/api/v1/signin", async(req,res)=>{
+    const username = req.body.username;
+    const password = req.body.password;
+    const existingUser = await UserModel.findOne({
+        username,
+        password
+    })
+
+    if(existingUser){
+        const token = jwt.sign({
+            id : existingUser._id
+        },JWT_PASSWORD)
+
+        res.json({
+            token
+        })
+    }else{
+        res.status(403).json({
+            message : "Incorrect crendentials "
+        })
+    }
 })
-app.post("/api/v1/content",(req,res)=>{
+app.post("/api/v1/signup",async (req,res)=>{
+    //TODO : zod validations , hash the passwords , if user already exists then return 411 status code and system should not crash 
+    const username = req.body.username;
+    const password = req.body.password;
+
+    try {
+        await UserModel.create({
+            username : username,
+            password : password
+        })
+    
+        res.json({
+            message: "User signed up"
+        })
+    } catch (error) {
+        res.status(411).json({
+            message : "User already exists"
+        })
+        
+    }
+    
 
 })
 app.get("/api/v1/content",(req,res)=>{
@@ -29,6 +73,6 @@ app.get("api/v1/brain/:shareLink",(req,res)=>{
 })
 
 
-
+app.listen(3000);
 
 
